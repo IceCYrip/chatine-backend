ede93f6e7311525658512bfc42b461a179b8e529
IceCYrip <karansable16@gmail.com>
2024-01-23 01:46:01 +0530

    cors access for all (try 3)

--- index.js
+++ index.js
@@ -7,16 +7,7 @@ const port = 5000
 
 app.use(express.json())
 
-// app.use(cors())
-
-app.use(
-  cors({
-    origin: 'http://localhost:3000', // or true to allow all origins
-    methods: 'GET,HEAD,OPTIONS,POST,PUT',
-    allowedHeaders:
-      'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization',
-  })
-)
+app.use(cors())
 
 // Available Routes
 
