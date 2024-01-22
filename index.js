4214423e50133329abd149874644ca131b3e1bc4
IceCYrip <karansable16@gmail.com>
2024-01-23 02:19:01 +0530

    explicitly set in api (try 6)

--- index.js
+++ index.js
@@ -25,7 +25,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use('/api/auth', require('./routes/auth'))
+app.use(
+  '/api/auth',
+  (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+  },
+  require('./routes/auth')
+)
 
 app.listen(port, () => {
   console.log(`Chatine - AI chat app listening on port ${port}`)
