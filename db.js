3bb2616e5963956e7ee1ace5fd02e822cd00462d
IceCYrip <karansable16@gmail.com>
2024-01-23 16:20:57 +0530

    I dont know what i am doing

--- db.js
+++ db.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const mongoURI =
   "mongodb+srv://chatine322519:chatineUser123@chatine.ff8y0tk.mongodb.net/chatine";
 const connectToMongo = () => {
-  mongoose.connect(mongoURI);
+  mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10000, // Set a longer timeout value
+  });
 };
 
 module.exports = connectToMongo;
